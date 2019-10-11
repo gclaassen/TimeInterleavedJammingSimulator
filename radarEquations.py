8084ca766790dad30d14644cb42c16b09b922ee8
Gert Claassen <gertclaassen@outlook.com>
2019-10-11 13:47:31 +0200

    Update radarEquations.py

--- radarEquations.py
+++ radarEquations.py
@@ -1,6 +1,6 @@
 import math
 
-def setERPdBm(power_W, gain):
+def convertERPdBm(power_W, gain):
  return convertPower_WtodBm(power_W) + convertGain_dBm(gain)
 
 def convertPower_WtodBm(power_W):
@@ -8,3 +8,6 @@ def convertPower_WtodBm(power_W):
 
 def convertGain_dBm(gain):
   return 10*math.log10(gain)
+
+def skinReturnPower(Threat_Tx_ERP, Threat_Tx_Fc,  Threat_Rx_GaindBm, Platform_RCS, Range, Range_losses):
+ return (Threat_Tx_ERP - 103 - 20*math.log(Threat_Tx_Fc) - 40*math.log(Range) + Threat_Rx_GaindBm + 10*math.log(Platform_RCS) + 20*log(Range_losses))
