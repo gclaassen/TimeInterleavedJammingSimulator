1afc05eda05effe54a764ad1ea6bb8a9be8d46b8
Gert Claassen <gertclaassen@outlook.com>
2020-06-25 15:23:05 +0200

    wip ranking

--- tij_ranking.py
+++ tij_ranking.py
@@ -1,12 +1,32 @@
 import numpy as np
 
-def rankingTest1(categoriesList, p):
+def ranking(categoriesList, p):
+    sumVal = 0
+    n = categoriesList.__len__()
     for xk in categoriesList:
-        sgn(xk)*np.pow()
+        sumVal += sgn(xk)*np.abs(xk)**(1/p)
+    return ((1/n)*sumVal)**p
 
 
-def rankingTest1Weigthed(categoriesList, weightList, p):
-    pass
+def rankingWeigthed(weigthedCategoriesList, p):
+    weight = 0
+    x = 1
+    sumVal = 0
+    weigthVal = 0
+
+    for xk in weigthedCategoriesList:
+        weigthVal += xk[weight]
+        sumVal += xk[weight]*sgn(xk[x])*np.abs(xk[x])**(1/p)
+
+    return ((1/weigthVal)*sumVal)**p
+
+def sgn(x):
+    if x<0:
+        return -1
+    if x==0:
+        return 0
+    if x>1:
+        return 1
 
 def profileCreatorInterval():
     pass
