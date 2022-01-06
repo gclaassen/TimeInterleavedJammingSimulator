e596223dde6f0f834522efea1248b54c32dc380e
Gert Claassen <gertclaassen@outlook.com>
2022-01-06 08:01:24 +0200

    update correct intervals for heatmaps

--- __explorative/visualize.py
+++ __explorative/visualize.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from tkinter import filedialog
 from tkinter import *
 from matplotlib import colors
+import os
 
 
 RESULTMODESLOG = 'modesChangeLog'
@@ -18,7 +19,7 @@ RESULTFILEEXT = '.npy'
 
 fontSize_tics = 8
 
-def plotModes(folder_selected):
+def plotModes(folder_selected, testName):
     modes = np.load(folder_selected+'/'+RESULTMODESLOG+RESULTFILEEXT)
     yticklabels = np.arange(1, len(modes)+1, 2)
     plt.figure()
@@ -32,46 +33,54 @@ def plotModes(folder_selected):
     colorbar.set_ticklabels(['TS', 'TA', 'TT', 'MG'])
     plt.xticks(fontsize= fontSize_tics)
     plt.yticks(fontsize= fontSize_tics)
-    plt.xlabel('Intervals', fontsize = fontSize_tics)
+    plt.xlabel('Interval', fontsize = fontSize_tics)
     plt.ylabel('Radars', fontsize = fontSize_tics)
-    plt.savefig('__modes.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
+    plt.savefig(testName + '__modes.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
 
-def plotCoincidenceRate(folder_selected):
+def plotCoincidenceRate(folder_selected, testName):
     coincidences = np.load(folder_selected+'/'+RESULTCOINCIDENCEPERCENTAGELOG+RESULTFILEEXT)
     yticklabels = np.arange(1, len(coincidences)+1, 2)
+    ___, totIntervals = coincidences.shape
+    xtickLables = np.arange(1, totIntervals, 3)
     plt.figure()
     ax2 = plt.gca()
     sb.set(font_scale=0.6)
     sb.set_style("ticks")
     sb.heatmap(coincidences, cmap="flare", square=True, linewidth=0.1, linecolor='w', ax=ax2, xticklabels = 3, yticklabels = 2, cbar_kws={"shrink": 0.35, "pad":0.01})
     ax2.set_yticklabels(yticklabels, rotation='horizontal')
+    ax2.set_xticklabels(xtickLables)
     colorbar = ax2.collections[0].colorbar
     plt.xticks(fontsize= fontSize_tics)
     plt.yticks(fontsize= fontSize_tics)
-    plt.xlabel('Intervals', fontsize= fontSize_tics)
+    plt.xlabel('Interval', fontsize= fontSize_tics)
     plt.ylabel('Radars', fontsize= fontSize_tics)
-    plt.savefig('__coincRate.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
+    plt.savefig(testName + '__coincRate.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
 
 
-def plotJammingPercRate(folder_selected):
+def plotJammingPercRate(folder_selected, testName):
     jamming = np.load(folder_selected+'/'+RESULTJAMMINGLOG+RESULTFILEEXT)
     yticklabels = np.arange(1, len(jamming)+1, 2)
+    ___, totIntervals = jamming.shape
+    xtickLables = np.arange(1, totIntervals, 3)
     plt.figure()
     ax3 = plt.gca()
     sb.set(font_scale=0.6)
     sb.set_style("ticks")
     sb.heatmap(jamming, cmap="coolwarm", square=True, linewidth=0.1, linecolor='w', ax=ax3, vmin=0, xticklabels = 3, yticklabels= 2, cbar_kws={"shrink": 0.35, "pad":0.01})
     ax3.set_yticklabels(yticklabels, rotation='horizontal')
+    ax3.set_xticklabels(xtickLables)
     colorbar = ax3.collections[0].colorbar
     plt.xticks(fontsize= fontSize_tics)
     plt.yticks(fontsize= fontSize_tics)
-    plt.xlabel('Intervals', fontsize= fontSize_tics)
+    plt.xlabel('Interval', fontsize= fontSize_tics)
     plt.ylabel('Radars', fontsize= fontSize_tics)
-    plt.savefig('__jammingPerc.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
+    plt.savefig(testName + '__jammingPerc.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
 
-def plotZA(folder_selected):
+def plotZA(folder_selected, testName):
     za = np.load(folder_selected+'/'+RESULTRANGELOG+RESULTFILEEXT)
     yticklabels = np.arange(1, len(za)+1, 2)
+    ___, totIntervals = za.shape
+    xtickLables = np.arange(1, totIntervals, 3)
     plt.figure()
     ax4 = plt.gca()
     sb.set(font_scale=0.6)
@@ -79,16 +88,19 @@ def plotZA(folder_selected):
     za_colors=['#329932', '#236B23', '#ffa700', '#b37400', '#fb4340', '#962826']
     sb.heatmap(za, cmap=za_colors, square=True, linewidth=0.1, linecolor='w', ax=ax4, vmin=0.7, xticklabels = 3, yticklabels= 2, cbar_kws={"shrink": 0.35, "pad":0.01})
     ax4.set_yticklabels(yticklabels, rotation='horizontal')
+    ax4.set_xticklabels(xtickLables)
     colorbar = ax4.collections[0].colorbar
     plt.xticks(fontsize= fontSize_tics)
     plt.yticks(fontsize= fontSize_tics)
-    plt.xlabel('Intervals', fontsize= fontSize_tics)
+    plt.xlabel('Interval', fontsize= fontSize_tics)
     plt.ylabel('Radars', fontsize= fontSize_tics)
-    plt.savefig('__za.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
+    plt.savefig(testName + '__za.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
 
-def plotWS(folder_selected):
+def plotWS(folder_selected, testName):
     Rws = np.load(folder_selected+'/'+RESULTLETHALRANGELOG+RESULTFILEEXT)
     yticklabels = np.arange(1, len(Rws)+1, 2)
+    ___, totIntervals = Rws.shape
+    xtickLables = np.arange(1, totIntervals, 3)
     plt.figure()
     ax5 = plt.gca()
     sb.set(font_scale=0.6)
@@ -96,14 +108,15 @@ def plotWS(folder_selected):
     Rws_colors=['#008000', '#D11919']
     sb.heatmap(Rws, cmap=Rws_colors, square=True, linewidth=0.1, linecolor='w', ax=ax5, vmin=0, vmax=1, xticklabels = 3, yticklabels= 2, cbar_kws={"shrink": 0.35, "pad":0.01})
     ax5.set_yticklabels(yticklabels, rotation='horizontal')
+    ax5.set_xticklabels(xtickLables)
     colorbar = ax5.collections[0].colorbar
     colorbar.set_ticks([0.25, 0.75])
     colorbar.set_ticklabels(["Out range", "In range"])
     plt.xticks(fontsize= fontSize_tics)
     plt.yticks(fontsize= fontSize_tics)
-    plt.xlabel('Intervals', fontsize= fontSize_tics)
+    plt.xlabel('Interval', fontsize= fontSize_tics)
     plt.ylabel('Radars', fontsize= fontSize_tics)
-    plt.savefig('__ws.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
+    plt.savefig(testName + '__ws.pdf', bbox_inches='tight',pad_inches = 0, dpi = 600)
 
 def calculateStats(folder_selected):
     modes = np.load(folder_selected+'/'+RESULTMODESLOG+RESULTFILEEXT)
@@ -140,13 +153,15 @@ def main():
     root.withdraw()
     folder_selected = filedialog.askdirectory()
 
+    testName = os.path.basename(folder_selected)
+
     calculateStats(folder_selected)
 
-    plotCoincidenceRate(folder_selected)
-    plotJammingPercRate(folder_selected)
-    plotZA(folder_selected)
-    plotWS(folder_selected)
-    plotModes(folder_selected)
+    plotCoincidenceRate(folder_selected, testName)
+    plotJammingPercRate(folder_selected, testName)
+    plotZA(folder_selected, testName)
+    plotWS(folder_selected, testName)
+    plotModes(folder_selected, testName)
 
 if __name__ == "__main__":
     main()
