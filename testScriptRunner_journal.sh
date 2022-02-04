02d776fdd6c05545528ec3885975dcff148b82e6
Gert Claassen <gclaassen@csir.co.za>
2022-02-04 15:50:10 +0200

    run all tests

--- testScriptRunner_journal.sh
+++ testScriptRunner_journal.sh
@@ -1,19 +1,19 @@
 # !/bin/bash
 
-# echo "Running Test 1/9: JournalRandomStart_TT_0PRI"
-# python 2_code/main.py -i JournalRandomStart_TT_0PRI -m 0.5 -z 0.5 -l 0.0 -j 0.0 -t -r
+echo "Running Test 1/9: JournalRandomStart_TT_0PRI"
+python 2_code/main.py -i JournalRandomStart_TT_0PRI -m 0.5 -z 0.5 -l 0.0 -j 0.0 -t -r
 
-# echo "Running Test 2/9: JournalRandomStart_TT_0PRI_ModeZaLr"
-# python 2_code/main.py -i JournalRandomStart_TT_0PRI_ModeZaLr -m 0.33 -z 0.33 -l 0.34 -j 0.0 -t -r
+echo "Running Test 2/9: JournalRandomStart_TT_0PRI_ModeZaLr"
+python 2_code/main.py -i JournalRandomStart_TT_0PRI_ModeZaLr -m 0.33 -z 0.33 -l 0.34 -j 0.0 -t -r
 
-# echo "Running Test 3/9: JournalRandomStart_TT_0PRI_modeTest"
-# python 2_code/main.py -i JournalRandomStart_TT_0PRI_modeTest -m 0.25 -z 0.25 -l 0.25 -j 0.25 -t -r
+echo "Running Test 3/9: JournalRandomStart_TT_0PRI_modeTest"
+python 2_code/main.py -i JournalRandomStart_TT_0PRI_modeTest -m 0.25 -z 0.25 -l 0.25 -j 0.25 -t -r
 
-# echo "Running Test 4/9: JournalRandomStart_TT_0PRI_IJonly"
-# python 2_code/main.py -i JournalRandomStart_TT_0PRI_IJonly -m 0.0 -z 0.0 -l 0.0 -j 1.0 -t -r
+echo "Running Test 4/9: JournalRandomStart_TT_0PRI_IJonly"
+python 2_code/main.py -i JournalRandomStart_TT_0PRI_IJonly -m 0.0 -z 0.0 -l 0.0 -j 1.0 -t -r
 
-# echo "Running Test 5/9: JournalRandomStart_TT_0PRI_IJhighest"
-# python 2_code/main.py -i JournalRandomStart_TT_0PRI_IJhighest -m 0.15 -z 0.05 -l 0.1 -j 0.7 -t -r
+echo "Running Test 5/9: JournalRandomStart_TT_0PRI_IJhighest"
+python 2_code/main.py -i JournalRandomStart_TT_0PRI_IJhighest -m 0.15 -z 0.05 -l 0.1 -j 0.7 -t -r
 
 echo "Running Test 6/9: JournalRandomStart_TT_10PRI"
 python 2_code/main.py -i JournalRandomStart_TT_10PRI -m 0.15 -z 0.05 -l 0.1 -j 0.7 -p -t -r
